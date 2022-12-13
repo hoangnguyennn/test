3287dcec1e779b30b3b21fe137ca6735d9c2a724
hoangnguyennn <nvanhoang188@gmail.com>
2022-12-13 16:58:11 +0700

    create AboutUs component

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,10 @@ export default {
 
 <script setup lang="ts">
 import Hero from '~/components/pages/landing/Hero.vue'
+import AboutUs from './components/pages/landing/AboutUs.vue'
 </script>
 
 <template>
   <Hero />
+  <AboutUs />
 </template>
---
+++ src/components/pages/landing/AboutUs.vue
@@ -0,0 +1,235 @@
+<script lang="ts">
+export default {
+  name: 'AboutUs'
+}
+</script>
+
+<script setup lang="ts">
+import calendarTickIcon from '~/assets/images/calendar_tick.svg'
+import penToolIcon from '~/assets/images/pen_tool.svg'
+import peopleIcon from '~/assets/images/people.svg'
+import witchImg from '~/assets/images/witch.png'
+import mapImg from '~/assets/images/pin_map.png'
+</script>
+
+<template>
+  <div class="about-us spacer-48">
+    <div class="title-and-description">
+      <h3 class="title">About us</h3>
+      <p class="description">
+        Browse our selection of free online games and have a great time without
+        leaving the site! Our Kids Games option also includes game reviews,
+        extensive game cheats and walkthroughs, and much more. We have exclusive
+        free downloads, videos, and articles as well. Etech reviews the most
+        popular kids games from all the most popular video gaming platforms, so
+        you don’t need to search around for fun anywhere else on the Internet.
+        Explore a whole new world of gaming on Etech.
+      </p>
+    </div>
+
+    <div class="statistics">
+      <div class="statistic">
+        <div class="title">
+          <span>600</span>
+          <span>M</span>
+          <span>+</span>
+        </div>
+        <div class="description">Users</div>
+      </div>
+      <div class="statistic">
+        <div class="title">
+          <span>135</span>
+          <span>+</span>
+        </div>
+        <div class="description">Games</div>
+      </div>
+    </div>
+
+    <div class="benefits-box">
+      <div class="benefits">
+        <div class="benefit">
+          <div class="icon">
+            <div class="wrap">
+              <img :src="calendarTickIcon" alt="" />
+            </div>
+          </div>
+          <div class="text">
+            <div class="title">24 - Hour</div>
+            <div class="content">
+              24/7 access ensures operators' businesses runs smoothly all year
+              long.
+            </div>
+          </div>
+        </div>
+
+        <div class="benefit">
+          <div class="icon">
+            <div class="wrap">
+              <img :src="penToolIcon" alt="" />
+            </div>
+          </div>
+          <div class="text">
+            <div class="title">Design</div>
+            <div class="content">
+              Combining imaginative universes, play dynamics, and unprecedented
+              gameplay, our games transcend the boundaries of the virtual world
+              by weaving innovative gameplay.
+            </div>
+          </div>
+        </div>
+
+        <div class="benefit">
+          <div class="icon">
+            <div class="wrap">
+              <img :src="peopleIcon" alt="" />
+            </div>
+          </div>
+          <div class="text">
+            <div class="title">Team</div>
+            <div class="content">
+              Etech is an award-winning, international studio of designers,
+              artists, animators and producers that create content for the
+              biggest names in film and video games.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="witch-and-map">
+      <div class="witch">
+        <img :src="witchImg" alt="" />
+      </div>
+      <div class="map">
+        <img :src="mapImg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.about-us {
+  .title-and-description {
+    padding-left: 16px;
+    padding-right: 16px;
+
+    .title {
+      font-family: 'Playfair Display';
+      font-size: 40px;
+      font-weight: 900;
+      line-height: 50px;
+      letter-spacing: 0.6px;
+      color: #000000;
+    }
+
+    .description {
+      color: #757575;
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 140%;
+    }
+  }
+
+  .statistics {
+    display: flex;
+    padding-left: 16px;
+    padding-right: 16px;
+    padding-bottom: 40px;
+
+    .statistic {
+      .title {
+        font-weight: 700;
+        letter-spacing: -1px;
+        color: #079bee;
+
+        span:nth-child(1) {
+          font-size: 44px;
+          line-height: 54px;
+        }
+
+        span:nth-child(2) {
+          font-size: 20px;
+          line-height: 24px;
+        }
+
+        span:last-child {
+          font-size: 36px;
+          line-height: 44px;
+        }
+      }
+
+      .description {
+        color: #000;
+        font-size: 24px;
+        font-weight: 700;
+        line-height: 30px;
+        width: 164px;
+      }
+    }
+  }
+
+  .benefits-box {
+    background-color: #eee;
+
+    .benefits {
+      padding: 40px 16px;
+
+      .benefit {
+        display: flex;
+
+        .icon {
+          width: 60px;
+
+          .wrap {
+            width: 40px;
+            height: 40px;
+            background-color: #e3fcff;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+        }
+
+        .text {
+          flex: 1;
+
+          .title {
+            font-size: 24px;
+            font-weight: 700;
+            line-height: 30px;
+          }
+
+          .content {
+            margin-top: 12px;
+            color: #757575;
+            font-size: 14px;
+            font-weight: 400;
+            line-height: 140%;
+          }
+        }
+
+        &:not(:last-child) {
+          margin-bottom: 40px;
+        }
+      }
+    }
+  }
+
+  .witch-and-map {
+    padding-top: 40px;
+
+    .witch {
+      padding-left: 42px;
+      padding-right: 33px;
+
+      position: relative;
+      z-index: 1;
+    }
+
+    .map {
+      margin-top: -31px;
+    }
+  }
+}
+</style>
